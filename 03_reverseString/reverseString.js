4fa1af17e881e7fc8a1a2e31cc62efdce51c436b
Raif Shareef <raif.sh94@gmail.com>
2025-01-27 00:52:03 +0800

    js-solution-3 how to reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let length = string.length;
+    let reverseString = ""
+    for (let i = 0; i < length; i++){
+        reverseString += string[length - 1 - i]
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
