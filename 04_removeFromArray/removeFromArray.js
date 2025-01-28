545bdd660f53b1d0bcf2a8e3327b510c60a76077
Raif Shareef <raif.sh94@gmail.com>
2025-01-28 23:39:07 +0800

    js-4-solution learning array filter method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
 const removeFromArray = function() {
+    const args = [...arguments]
+    let array = args[0]
+    let remove = []
+
+    if (args.length > 1) {
+        for (let i = 1; i < args.length; i++){
+            remove.push(args[i]);
+            console.log(remove)
+        }
+    } else {
+        remove.push(args[1]);
+        console.log(remove)
+    }
+
+    let filteredArr = array.filter(element => !remove.includes(element));
+
+    return filteredArr
+
 };
 
 // Do not edit below this line
