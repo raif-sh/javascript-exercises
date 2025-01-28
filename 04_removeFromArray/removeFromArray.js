29cbe621edc844a3e6d08edb46abd1b4084fe46b
Raif Shareef <raif.sh94@gmail.com>
2025-01-28 23:59:37 +0800

    improving js 4 solution;

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,22 +1,18 @@
-const removeFromArray = function() {
-    const args = [...arguments]
-    let array = args[0]
-    let remove = []
+const removeFromArray = function(array, ...remove) {
+    // The 'array' parameter is the array we want to modify (the original array).
+    // The '...remove' parameter uses the rest operator to gather all additional
+    // arguments passed to the function into an array called 'remove'. These are the
+    // items we want to remove from 'array'.
 
-    if (args.length > 1) {
-        for (let i = 1; i < args.length; i++){
-            remove.push(args[i]);
-            console.log(remove)
-        }
-    } else {
-        remove.push(args[1]);
-        console.log(remove)
-    }
-
+    // Use the 'filter' method to create a new array that excludes the items in 'remove'.
+    // The 'filter' method goes through each element in 'array'.
+    // For each element, it checks if it's NOT included in the 'remove' array.
+    // If it's NOT in 'remove', it's kept in the new array. If it IS in 'remove', it's excluded.
     let filteredArr = array.filter(element => !remove.includes(element));
 
-    return filteredArr
-
+    // Return the new filtered array. This array contains only the elements
+    // from the original array that were NOT in the 'remove' array.
+    return filteredArr;
 };
 
 // Do not edit below this line
