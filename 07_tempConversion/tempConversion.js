c116506243043161c9fcd780616dca3e8e316c5a
Raif Shareef <raif.sh94@gmail.com>
2025-01-30 01:25:14 +0800

    js-7-solution using formula to convert

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  // °F ≘ (x − 32) × ⁠
+// 5
+// /
+// 9
+// ⁠ °C
+  let far = (temp - 32) * 5/9
+  far = parseFloat(far.toFixed(1));
+  return far;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  // °C ≘ (x × ⁠
+  //   9
+  //   /
+  //   5
+  //   ⁠ + 32) °F
+  let cel = (temp * 9 / 5 + 32)
+  cel = parseFloat(cel.toFixed(1));
+  return cel;
 };
 
 // Do not edit below this line
