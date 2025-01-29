3ac2556d636175fa8f4f37964c6e7ddba97c895b
Raif Shareef <raif.sh94@gmail.com>
2025-01-30 00:45:13 +0800

    js-6-solution working with remainder operator

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // Is year divisible by 4?
+    let divByFour = year % 4;
+    let divByHundred = year % 100;
+    let divByFourHundred = year % 400;
 
+    console.log(divByFourHundred)
+    if (divByFour === 0 && divByHundred !== 0 ) {
+        return true;
+    } else if (divByFourHundred === 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
