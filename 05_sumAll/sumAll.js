689d589bcdc45eafa537ca38c10cb27ef63c6eef
Raif Shareef <raif.sh94@gmail.com>
2025-01-29 19:12:24 +0800

    js-05-solution learning to create a range array to sum all integers between a start and end number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,40 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    // This function will return the total sum of the starting number 
+    // to endNum by adding up all the integers between start and end.
 
+    // Create a variable to hold the final sum
+    let sum = 0;
+    let start = 0;
+    let end = 0;
+    let startIntCheck = Number.isInteger(startNum);
+    let endIntCheck = Number.isInteger(endNum);
+
+    if (startNum < 0 || endNum < 0 || startIntCheck === false || endIntCheck === false){
+        return "ERROR"
+    }
+
+    if (startNum > endNum){
+        start = endNum
+        end = startNum
+    } else {
+        start = startNum
+        end = endNum
+    }
+
+    // find range of numbers 
+    let range = [];
+    for (let i = start; i < end - 1; i++) {
+        range.push(i+1)
+    }
+
+    // Create a loop to go through each number
+    for (let i = 0; i < range.length; i++) {
+        sum += range[i];
+    }
+    sum += (startNum + endNum)
+
+
+    return sum;
 };
 
 // Do not edit below this line
