1df4d14da328453bad52cef82807d994848fb5fd
Raif Shareef <raif.sh94@gmail.com>
2025-01-30 22:57:14 +0800

    js-09-solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,18 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    let punctuation = /[\.,?!]/g;
+    // Remove punctuations
+    let newText = text.replace(punctuation, "");
+    // Remove whitespace and everything in lowercase
+    newText = newText.split(" ").join("").toLowerCase();
+
+    let len = newText.length;
+    
+    for(let i = 0; i < len; i++){
+        if (newText[i] !== newText[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 
 };
 
