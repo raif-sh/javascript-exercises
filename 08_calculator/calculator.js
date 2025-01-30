cd80570d74576eded0c303da6ed930148e93825b
Raif Shareef <raif.sh94@gmail.com>
2025-01-30 13:37:07 +0800

    js-8-solution doing math for calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,59 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let calSum = 0;
+
+  for (let i = 0; i < numbers.length; i++)
+  {
+    calSum += numbers[i];
+  }
+
+  return calSum;
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+  let multiplied = 1;
 
+  for (let i = 0; i < numbers.length; i++){
+    multiplied *= numbers[i];
+  }
+
+  return multiplied;
 };
 
-const power = function() {
-	
+const power = function(num, topower) {
+  let powered = 1;
+	for(let i = 0; i < topower; i++){
+    powered *= num;
+  }
+  return powered;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1
+  }
+  // find the range between number and 0
+  let range = []
+
+  for (let i = num; i > 0; i--){
+    range.push(i);
+  }
+  
+  let factorized = 1;
+  // multiply all numbers in range
+  for (let i = 0; i < range.length; i++){
+    factorized *= range[i];
+  }
+
+  return factorized;
+
 };
 
 // Do not edit below this line
